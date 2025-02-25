16064fc0bda6685a2c1f32cbe01a5214e9f713f1
Sebastián González Ríos <sebas@MacBook-Air-de-Sebas.local>
2025-02-25 03:31:50 +0100

    FEAT: add Next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunitoFont = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Simple list",
+  description: "Gestiona tu propia lista de manera sencilla.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body
+        className={`${nunitoFont.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-2xl">¡Hola mundo!</h1>
+    </div>
+  );
+}
